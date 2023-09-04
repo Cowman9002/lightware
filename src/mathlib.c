c0244c6174df7bf427b7c56fc3528e9cec325c3c
Cowman9002 <54774464+Cowman9002@users.noreply.github.com>
2023-09-04 19:45:31 -0400

    refactor: math library conversion to 3d

--- src/mathlib.c
+++ src/mathlib.c
@@ -1,6 +1,6 @@
 #include "mathlib.h"
 
-#include "math.h"
+#include <math.h>
 
 float dot2d(vec2 a, vec2 b) {
     return a[0] * b[0] + a[1] * b[1];
@@ -11,7 +11,7 @@ float cross2d(vec2 a, vec2 b) {
 }
 
 float cross32d(vec2 a, vec2 b, vec2 c) {
-  return (b[0] - a[0])*(c[1] - a[1]) - (b[1] - a[1])*(c[0] - a[0]);
+    return (b[0] - a[0]) * (c[1] - a[1]) - (b[1] - a[1]) * (c[0] - a[0]);
 }
 
 float dist2d(vec2 a, vec2 b) {
@@ -49,69 +49,140 @@ float normalize3d(vec3 a) {
 }
 
 
-void mat3Translate(vec2 t, mat3 out) {
-    out[0 + 0 * 3] = 1.0f;
-    out[1 + 0 * 3] = 0.0f;
-    out[2 + 0 * 3] = t[0];
-    out[0 + 1 * 3] = 0.0f;
-    out[1 + 1 * 3] = 1.0f;
-    out[2 + 1 * 3] = t[1];
+void mat4Translate(vec3 t, mat4 out) {
+    out[0 + 0 * 4] = 1.0f;
+    out[1 + 0 * 4] = 0.0f;
+    out[2 + 0 * 4] = 0.0f;
+    out[3 + 0 * 4] = t[0];
+
+    out[0 + 1 * 4] = 0.0f;
+    out[1 + 1 * 4] = 1.0f;
+    out[2 + 1 * 4] = 0.0f;
+    out[3 + 1 * 4] = t[1];
+
+    out[0 + 2 * 4] = 0.0f;
+    out[1 + 2 * 4] = 0.0f;
+    out[2 + 2 * 4] = 1.0f;
+    out[3 + 2 * 4] = t[2];
+
+    out[0 + 3 * 4] = 0.0f;
+    out[1 + 3 * 4] = 0.0f;
+    out[2 + 3 * 4] = 0.0f;
+    out[3 + 3 * 4] = 1.0f;
 }
 
-void mat3Rotate(float r, mat3 out) {
-    float c        = cosf(r);
-    float s        = sinf(r);
-    out[0 + 0 * 3] = c;
-    out[1 + 0 * 3] = -s;
-    out[2 + 0 * 3] = 0.0f;
-    out[0 + 1 * 3] = s;
-    out[1 + 1 * 3] = c;
-    out[2 + 1 * 3] = 0.0f;
+void mat4Scale(vec3 s, mat4 out) {
+    out[0 + 0 * 4] = s[0];
+    out[1 + 0 * 4] = 0.0f;
+    out[2 + 0 * 4] = 0.0f;
+    out[3 + 0 * 4] = 0.0f;
+
+    out[0 + 1 * 4] = 0.0f;
+    out[1 + 1 * 4] = s[1];
+    out[2 + 1 * 4] = 0.0f;
+    out[3 + 1 * 4] = 0.0f;
+
+    out[0 + 2 * 4] = 0.0f;
+    out[1 + 2 * 4] = 0.0f;
+    out[2 + 2 * 4] = s[2];
+    out[3 + 2 * 4] = 0.0f;
+
+    out[0 + 3 * 4] = 0.0f;
+    out[1 + 3 * 4] = 0.0f;
+    out[2 + 3 * 4] = 0.0f;
+    out[3 + 3 * 4] = 1.0f;
 }
 
-void mat3Scale(vec2 s, mat3 out) {
-    out[0 + 0 * 3] = s[0];
-    out[1 + 0 * 3] = 0.0f;
-    out[2 + 0 * 3] = 0.0f;
-    out[0 + 1 * 3] = 0.0f;
-    out[1 + 1 * 3] = s[1];
-    out[2 + 1 * 3] = 0.0f;
+void mat4RotateX(float r, mat4 out) {
+    float c = cosf(r);
+    float s = sinf(r);
+
+    out[0 + 0 * 4] = 1.0f;
+    out[1 + 0 * 4] = 0.0f;
+    out[2 + 0 * 4] = 0.0f;
+    out[3 + 0 * 4] = 0.0f;
+
+    out[0 + 1 * 4] = 0.0f;
+    out[1 + 1 * 4] = c;
+    out[2 + 1 * 4] = s;
+    out[3 + 1 * 4] = 0.0f;
+
+    out[0 + 2 * 4] = 0.0f;
+    out[1 + 2 * 4] = -s;
+    out[2 + 2 * 4] = c;
+    out[3 + 2 * 4] = 0.0f;
+
+    out[0 + 3 * 4] = 0.0f;
+    out[1 + 3 * 4] = 0.0f;
+    out[2 + 3 * 4] = 0.0f;
+    out[3 + 3 * 4] = 1.0f;
 }
 
-void mat3Inv(mat3 in, mat3 out) {
-    float a = in[0 + 0 * 3];
-    float b = in[1 + 0 * 3];
-    float c = in[2 + 0 * 3];
-    float d = in[0 + 1 * 3];
-    float e = in[1 + 1 * 3];
-    float f = in[2 + 1 * 3];
-    float g = 0.0f;
-    float h = 0.0f;
-    float i = 1.0f;
+void mat4RotateY(float r, mat4 out) {
+    float c = cosf(r);
+    float s = sinf(r);
 
-    float det     = a * (e * i - f * h) - b * (d * i - f * g) + c * (d * h - e * g);
-    float inv_det = 1.0f / det;
+    out[0 + 0 * 4] = c;
+    out[1 + 0 * 4] = 0.0f;
+    out[2 + 0 * 4] = -s;
+    out[3 + 0 * 4] = 0.0f;
 
-    out[0 + 0 * 3] = (e * i - f * h) * inv_det;
-    out[1 + 0 * 3] = -(b * i - c * h) * inv_det;
-    out[2 + 0 * 3] = (b * f - c * e) * inv_det;
-    out[0 + 1 * 3] = -(d * i - f * g) * inv_det;
-    out[1 + 1 * 3] = (a * i - c * g) * inv_det;
-    out[2 + 1 * 3] = -(a * f - c * d) * inv_det;
+    out[0 + 1 * 4] = 0.0f;
+    out[1 + 1 * 4] = 1.0f;
+    out[2 + 1 * 4] = 0.0f;
+    out[3 + 1 * 4] = 0.0f;
+
+    out[0 + 2 * 4] = s;
+    out[1 + 2 * 4] = 0.0f;
+    out[2 + 2 * 4] = c;
+    out[3 + 2 * 4] = 0.0f;
+
+    out[0 + 3 * 4] = 0.0f;
+    out[1 + 3 * 4] = 0.0f;
+    out[2 + 3 * 4] = 0.0f;
+    out[3 + 3 * 4] = 1.0f;
 }
 
-void mat3Mul(mat3 a, mat3 b, mat3 out) {
+void mat4RotateZ(float r, mat4 out) {
+    float c = cosf(r);
+    float s = sinf(r);
+
+    out[0 + 0 * 4] = c;
+    out[1 + 0 * 4] = s;
+    out[2 + 0 * 4] = 0.0f;
+    out[3 + 0 * 4] = 0.0f;
+
+    out[0 + 1 * 4] = -s;
+    out[1 + 1 * 4] = c;
+    out[2 + 1 * 4] = 0.0f;
+    out[3 + 1 * 4] = 0.0f;
+
+    out[0 + 2 * 4] = 0.0f;
+    out[1 + 2 * 4] = 0.0f;
+    out[2 + 2 * 4] = 1.0f;
+    out[3 + 2 * 4] = 0.0f;
+
+    out[0 + 3 * 4] = 0.0f;
+    out[1 + 3 * 4] = 0.0f;
+    out[2 + 3 * 4] = 0.0f;
+    out[3 + 3 * 4] = 1.0f;
+}
+
+void mat4Mul(mat4 a, mat4 b, mat4 out) {
     // c[ji] = a[0i] * b[j0] + a[1i] * b[j1] + a[2i] * b[j2]
-    out[0 + 0 * 3] = a[0 + 0 * 3] * b[0 + 0 * 3] + a[1 + 0 * 3] * b[0 + 1 * 3];
-    out[1 + 0 * 3] = a[0 + 0 * 3] * b[1 + 0 * 3] + a[1 + 0 * 3] * b[1 + 1 * 3];
-    out[2 + 0 * 3] = a[0 + 0 * 3] * b[2 + 0 * 3] + a[1 + 0 * 3] * b[2 + 1 * 3] + a[2 + 0 * 3];
-
-    out[0 + 1 * 3] = a[0 + 1 * 3] * b[0 + 0 * 3] + a[1 + 1 * 3] * b[0 + 1 * 3];
-    out[1 + 1 * 3] = a[0 + 1 * 3] * b[1 + 0 * 3] + a[1 + 1 * 3] * b[1 + 1 * 3];
-    out[2 + 1 * 3] = a[0 + 1 * 3] * b[2 + 0 * 3] + a[1 + 1 * 3] * b[2 + 1 * 3] + a[2 + 1 * 3];
+    for (unsigned c = 0; c < 4; c++) {
+        for (unsigned r = 0; r < 4; r++) {
+            out[c + r * 4] = a[0 + r * 4] * b[c + 0 * 4] +
+                             a[1 + r * 4] * b[c + 1 * 4] +
+                             a[2 + r * 4] * b[c + 2 * 4] +
+                             a[3 + r * 4] * b[c + 3 * 4];
+        }
+    }
 }
 
-void mat3MulVec2(mat3 a, vec2 b, vec2 out) {
-    out[0] = a[0 + 0 * 3] * b[0] + a[1 + 0 * 3] * b[1] + a[2 + 0 * 3];
-    out[1] = a[0 + 1 * 3] * b[0] + a[1 + 1 * 3] * b[1] + a[2 + 1 * 3];
+float mat4MulVec3(mat4 a, vec3 b, vec3 out) {
+    out[0] = a[0 + 0 * 4] * b[0] + a[1 + 0 * 4] * b[1] + a[2 + 0 * 4] * b[2] + a[3 + 0 * 4];
+    out[1] = a[0 + 1 * 4] * b[0] + a[1 + 1 * 4] * b[1] + a[2 + 1 * 4] * b[2] + a[3 + 1 * 4];
+    out[2] = a[0 + 2 * 4] * b[0] + a[1 + 2 * 4] * b[1] + a[2 + 2 * 4] * b[2] + a[3 + 2 * 4];
+    return a[0 + 3 * 4] * b[0] + a[1 + 3 * 4] * b[1] + a[2 + 3 * 4] * b[2] + a[3 + 3 * 4];
 }
--- src/mathlib.h
+++ src/mathlib.h
@@ -5,6 +5,7 @@
 #define TO_RADS (M_PI / 180.0)
 #define TO_DEGS (180.0 / M_PI)
 
+#define signum(a) (((a) > 0) - ((a) < 0))
 #define min(a, b) (((a) < (b)) ? (a) : (b))
 #define max(a, b) (((a) > (b)) ? (a) : (b))
 #define clamp(a, b, c) ((a) < (b) ? (b) : (a) > (c) ? (c) : (a))
@@ -13,14 +14,8 @@
 #define swap(T, x, y) do { T tmp = x; x = y; y = tmp; } while (0)
 
 typedef float vec2[2];
-typedef int vec2i[2];
 typedef float vec3[3];
-typedef float mat3[3 * 2]; // bottom layer is always 0, 0, 1
-
-#define VEC2(x, y) ((vec2){(x), (y)})
-#define VEC3(x, y, z) ((vec3){(x), (y), (z)})
-
-#define signum(a) (((a) > 0) - ((a) < 0))
+typedef float mat4[16];
 
 float dot2d(vec2 a, vec2 b);
 float cross2d(vec2 a, vec2 b);
@@ -32,10 +27,13 @@ float dist3d(vec3 a, vec3 b);
 float normalized3d(vec3 a, vec3 o);
 float normalize3d(vec3 a);
 
-void mat3Translate(vec2 t, mat3 out);
-void mat3Rotate(float r, mat3 out);
-void mat3Scale(vec2 s, mat3 out);
+void mat4Translate(vec3 t, mat4 out);
+void mat4Scale(vec3 s, mat4 out);
+void mat4RotateX(float r, mat4 out);
+void mat4RotateY(float r, mat4 out);
+void mat4RotateZ(float r, mat4 out);
 
-void mat3Inv(mat3 a, mat3 out);
-void mat3Mul(mat3 a, mat3 b, mat3 out);
-void mat3MulVec2(mat3 a, vec2 b, vec2 out);
+void mat4Mul(mat4 a, mat4 b, mat4 out);
+
+// returns w component of vec4
+float mat4MulVec3(mat4 a, vec3 b, vec3 out);
